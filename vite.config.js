5abecd451edfe76de4afaef74975f0ad20853a01
yamini1505 <nagothuyamini4848@gmail.com>
2025-08-01 11:33:43 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // default, but you can explicitly define it
+  },
+  server: {
+    port: 3000,
+  },
 })
